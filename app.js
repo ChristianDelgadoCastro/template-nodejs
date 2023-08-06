8af7f44449bb077dfa59b7ddace6589cfd46b689
Christian.exe <100442428+ChristianDelgadoCastro@users.noreply.github.com>
2023-08-05 20:26:48 -0600

    Adding a Pool to DB

--- app.js
+++ app.js
@@ -1,44 +1,32 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 const mysql = require('mysql2');
 
-// Configurar la conexión a la base de datos MySQL en la nube
-const dbConfig = {
+// Configurar la piscina de conexiones a la base de datos MySQL en la nube
+const dbPool = mysql.createPool({
     host: 'by9wewrbsgh2joxudcpi-mysql.services.clever-cloud.com',
     user: 'uy44aub6lptawz2c',
     password: '4t03eIWNHgChQqYtypHF',
-    database: 'by9wewrbsgh2joxudcpi', // Cambia esto al nombre de tu base de datos
-  };
-
-  
-// Crear la conexión a la base de datos
-const connection = mysql.createConnection(dbConfig);
-
-// Conectar a la base de datos
-connection.connect((err) => {
-    if (err) {
-        console.error('Error al conectar a la base de datos:', err);
-        console.log(variables.env.DB_HOST, variables.env.DB_USER, variables.env.DB_PASSWORD, variables.env.DB_DATABASE)
-    } else {
-        console.log('Conexión a la base de datos exitosa!');
-    }
+    database: 'by9wewrbsgh2joxudcpi',
+    connectionLimit: 10, // Número máximo de conexiones en la piscina
 });
+
+app.use(express.json());
+
 
 // Ruta de inicio de sesión
 app.post('/login', (req, res) => {
-    const { email, password } = req.body; // Asegúrate de enviar email y password en el cuerpo de la solicitud
+    const { email, password } = req.body;
 
-    // Verificar las credenciales en la base de datos
     const query = `SELECT * FROM alumnos WHERE email = '${email}' AND password = '${password}'`;
-    connection.query(query, (err, result) => {
+    dbPool.query(query, (err, result) => {
         if (err) {
             console.error('Error al verificar las credenciales:', err);
             res.status(500).send('Error al verificar las credenciales');
         } else {
             if (result.length > 0) {
-                // Credenciales válidas, iniciar sesión y generar una sesión (por ejemplo, con una cookie)
                 res.status(200).send('Inicio de sesión exitoso');
             } else {
                 res.status(401).send('Credenciales inválidas');
@@ -50,7 +38,7 @@ app.post('/login', (req, res) => {
 // Obtener todas las calificaciones
 app.get('/calificaciones', (req, res) => {
     const query = 'SELECT * FROM calificaciones';
-    connection.query(query, (err, result) => {
+    dbPool.query(query, (err, result) => {
         if (err) {
             console.error('Error al obtener las calificaciones:', err);
             res.status(500).send('Error al obtener las calificaciones');
@@ -67,7 +55,7 @@ app.get('/calificaciones/:email', (req, res) => {
       SELECT * FROM calificaciones
       WHERE nControl IN (SELECT ncontrol FROM alumnos WHERE email = '${email}')
     `;
-    connection.query(query, (err, result) => {
+    dbPool.query(query, (err, result) => {
         if (err) {
             console.error('Error al obtener las calificaciones:', err);
             res.status(500).send('Error al obtener las calificaciones');
@@ -80,7 +68,7 @@ app.get('/calificaciones/:email', (req, res) => {
 // Obtener todos los alumnos
 app.get('/alumnos', (req, res) => {
     const query = 'SELECT * FROM alumnos';
-    connection.query(query, (err, result) => {
+    dbPool.query(query, (err, result) => {
         if (err) {
             console.error('Error al obtener los alumnos:', err);
             res.status(500).send('Error al obtener los alumnos');
@@ -89,6 +77,7 @@ app.get('/alumnos', (req, res) => {
         }
     });
 });
+
 
 app.use(express.static('public'))
 
